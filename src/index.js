fc52a0ad0ea234167e15e1bd658930070f130e91
Sahil Modi <modi.sahil.im@gmail.com>
2026-02-28 06:03:41 +0400

    Add question parsing for create command

--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ const command = args[0];
 console.log('QuickPoll CLI v1.0.0\n');
 
 if (command === 'create') {
-  console.log('Creating a new poll...');
-  console.log('(Feature coming in next milestone!)');
+  const question = args.slice(1).join(' ');
+  
+  if (!question) {
+    console.log('❌ Error: Please provide a question');
+    console.log('Usage: quickpoll create "Your question here"');
+  } else {
+    console.log('✅ Creating poll:');
+    console.log(`   Question: ${question}`);
+    console.log('\n(Poll storage coming next!)');
+  }
 } else if (command === 'help') {
   console.log('Available commands:');
-  console.log('  quickpoll create  - Create a new poll');
-  console.log('  quickpoll help    - Show this help message');
+  console.log('  quickpoll create "question"  - Create a new poll');
+  console.log('  quickpoll help               - Show this help message');
 } else {
   console.log('Type "quickpoll help" to see available commands');
 }
