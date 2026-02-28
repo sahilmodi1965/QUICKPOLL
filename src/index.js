66021df720947bfb7e5e70d85ced5d541c72bfa0
Sahil Modi <modi.sahil.im@gmail.com>
2026-02-28 06:01:16 +0400

    Add command parsing for help and create commands

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 #!/usr/bin/env node
 
-console.log('QuickPoll CLI v1.0.0');
-console.log('Ready to create polls!');
+const args = process.argv.slice(2);
+const command = args[0];
+
+console.log('QuickPoll CLI v1.0.0\n');
+
+if (command === 'create') {
+  console.log('Creating a new poll...');
+  console.log('(Feature coming in next milestone!)');
+} else if (command === 'help') {
+  console.log('Available commands:');
+  console.log('  quickpoll create  - Create a new poll');
+  console.log('  quickpoll help    - Show this help message');
+} else {
+  console.log('Type "quickpoll help" to see available commands');
+}
